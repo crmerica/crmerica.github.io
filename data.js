e4203e4074530f3a1074c6ab17861880278ca6e6
crmerica <52659485+crmerica@users.noreply.github.com>
2019-07-09 15:56:20 +0300

    Update data.js

--- data.js
+++ data.js
@@ -8,27 +8,25 @@ if (answerBot) {
     answerBot.prototype.keywords = [
         { "keys": ["hei"], "value": 0 },
         { "keys": ["päivää"], "value": 0 },
-        { "keys": ["life", "universe", "everything"], "value": 1 },
-        { "keys": ["software", "development"], "value": 2 },
-        { "keys": ["software", "engineering"], "value": 2 },
-        { "keys": ["who", "made", "you"], "value": 3 },
-        { "keys": ["who", "wrote", "you"], "value": 3 },
-        { "keys": ["who", "coded", "you"], "value": 3 },
-        { "keys": ["is", "this", "real", "life"], "value": 4 },
-        { "keys": ["who", "is", "deni"], "value": 5 },
-        { "keys": ["tell", "me", "about", "deni"], "value": 5 },
-        { "keys": ["tell", "me", "about", "author"], "value": 5 },
-	    { "keys": ["show", "me", "author"], "value": 5 },
-        { "keys": ["can", "see", "source"], "value": 6 },
-        { "keys": ["can", "see", "sourcecode"], "value": 6 },
-        { "keys": ["show", "me", "code"], "value": 6 },
-        { "keys": ["how", "are", "you"], "value": 7 },
+        { "keys": ["mitä", "tarkoittaa", "account"], "value": 1 },
+        { "keys": ["mikä", "contact", "kontakti"], "value": 2 },
+        { "keys": ["mitä", "tarkoittaa"], "value": 2 },
+        { "keys": ["mergaus", "mergaaminen", "merge"], "value": 3 },
+        { "keys": ["mikä", "on", "mergaus"], "value": 3 },
+        { "keys": ["duplikaatti", "duplikaatit", "yhdistäminen"], "value": 3 },
+        { "keys": ["mikä", "on", "duplikaatti", "CRMssä"], "value": 4 },
+        { "keys": ["miten", "haen", "tietoa"], "value": 5 },
+        { "keys": ["miten", "loydän", "tietoa", "CRMstä"], "value": 5 },
+        { "keys": ["mikä", "on", "erikoishaku"], "value": 6 },
+        { "keys": ["mikä", "on", "advanced search"], "value": 6 },
+        { "keys": ["miten", "käytän", "erikoishakua"], "value": 6 },
+        { "keys": ["mikä", "on", "lahjoittaja"], "value": 7 },
 	    { "keys": ["who", "is", "this"], "value": 8 }];
 
     answerBot.prototype.answers = [
         {
             "description": "Hei!",
-            "values": ["Hei!", "Hei, kuinka voin auttaa tänään", "Hei! Mikä mieltäsi askarruttaa CRM liittyen?"]
+            "values": ["Hei! Tässä on muutamia avainsanoja, jotka voivat auttaa sinua / kontakti, account, mergaaminen, lahjoittaja, tiedonhaku, erikoishaku", "Hei, tässä on muutamia avainsanoja, jotka voivat auttaa sinua / kontakti, account, mergaaminen, lahjoittaja, tiedonhaku, erikoishaku", "Hei! Mikä mieltäsi askarruttaa CRM liittyen? Anna avainsana muodossa kontakti, account jne."]
         },
         {
             "description": "Account",
@@ -55,12 +53,12 @@ if (answerBot) {
             "values": ["Erikoishaku tarkoittaa..."]
         },
         {
-            "description": "How are you?",
-            "values": ["I'm good how are you?"]
+            "description": "Lahjoittaja?",
+            "values": ["Lahjoittaja on..."]
         },
         {
-            "description": "Who is this?",
-            "values": ["StackOverflow Exception occurred", "The question is who are you?"]
+            "description": "Kenelle puhun?",
+            "values": ["Puhut CRM botille!"]
         }
         ];
 }
